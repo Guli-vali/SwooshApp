a2fd22dacc09f6ef83387087b8f02fe5cc0876e3
Илья Маслиев <ilamasliev@Ilas-MacBook-Air.local>
2018-01-31 00:11:51 +0300

    initial

---
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Илья Маслиев on 28/01/2018.
+//  Copyright © 2018 Masliev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgImg: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
