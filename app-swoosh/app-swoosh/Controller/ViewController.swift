295ed806ec59f990f48c3ed2a722ac77f4137e24
Илья Маслиев <ilamasliev@Ilas-MacBook-Air.local>
2018-02-03 11:22:15 +0300

    1

---
+++ app-swoosh/app-swoosh/Controller/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Илья Маслиев on 28/01/2018.
+//  Copyright © 2018 Masliev. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
+        
+    }
+
+
+}
+
---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Илья Маслиев on 01/02/2018.
+//  Copyright © 2018 Masliev. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
